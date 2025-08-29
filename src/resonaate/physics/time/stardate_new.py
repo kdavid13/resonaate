aa9a5f6d5efdb892bfe09ab86cc326dc93136455
David Kusterer <kdavid13@vt.edu>
2025-08-29 12:58:41 -0400

    make stardate classes inherit from builtins; tests pass within tolerance

--- src/resonaate/physics/time/stardate_new.py
+++ src/resonaate/physics/time/stardate_new.py
@@ -1,40 +1,28 @@
 """Defines a :class:`.Stardate` class to abstract an instant in time."""
 from __future__ import annotations
 
+# Standard Library Imports
 from datetime import datetime
 from enum import Enum
 
+# Third Party Imports
 import numpy as np
 
+# Local Imports
 from .. import constants as CONST
 
 AD_OFFSET: float = 1721424.5
 """Number of days from _noon_ of Janurary 1, 4713 BC to _midnight_ of January 1, 1 AD."""
 
 
-class DayFrac:
+class DayFrac(float):
     """Number of elapsed days since _noon_ of January 1, 4713 BC."""
-
-    def __init__(self, days_a: float, days_b: float = 0.0):
-        """Instantiate a :class:`.DayFrac` instance from a two-part Julian day number.
-
-        Args:
-            days_a: Portion (or whole part) of Julian day number being represented.
-            days_b: Portion (or whole part) of Julian day number being represented.
-        """
-        self._days_a = days_a
-        self._days_b = days_b
-
-    @property
-    def days(self) -> float:
-        """Returns the sum of the specified two-part Julian day number."""
-        return self._days_a + self._days_b
 
     def toDatetime(self) -> DatetimeExt:
         """Convert this :class:`.DayFrac` to a :class:`.DatetimeExt`."""
-        mod_ad_jd = self.days - AD_OFFSET
+        mod_ad_jd = self - AD_OFFSET
         mod_ad_day = int(np.floor(mod_ad_jd))
-        dt = datetime.fromordinal(mod_ad_day)
+        _date = datetime.fromordinal(mod_ad_day)
         day_frac = mod_ad_jd - mod_ad_day
 
         hour_remainder = day_frac * CONST.DAYS2HOUR
@@ -44,52 +32,24 @@ class DayFrac:
         second_remainder = (minute_remainder - minute) * CONST.MINUTE2SEC
         second = int(np.floor(second_remainder))
         micro = int(np.round((second_remainder - second) * 1e6))
-        dt = dt.replace(hour=hour, minute=minute, second=second, microsecond=micro)
-        return DatetimeExt(dt)
+        return DatetimeExt(_date.year, _date.month, _date.day, hour, minute, second, micro)
 
 
-class DatetimeExt:
+class DatetimeExt(datetime):
     """Extended functionality encapsulated with a Python `datetime`."""
 
-    def __init__(self, dt: datetime):
-        """Instantiate a :class:`.DatetimeExt` from specified `datetime`.
-
-        Args:
-            dt: Specified Python `datetime` object.
-        """
-        self._dt = dt
-
     @property
-    def year(self) -> int:
-        return self._dt.year
-
-    @property
-    def month(self) -> int:
-        return self._dt.month
-
-    @property
-    def day(self) -> int:
-        return self._dt.day
-
-    @property
-    def hour(self) -> int:
-        return self._dt.hour
-
-    @property
-    def minute(self) -> int:
-        return self._dt.minute
-
-    @property
-    def second(self) -> float:
-        return self._dt.second + self._dt.microsecond * 1e-6
+    def sec_frac(self) -> float:
+        """Converts internal :attr:`.microsecond` value to floating point second fraction."""
+        return self.microsecond * 1e-6
 
     def toDayFrac(self) -> DayFrac:
         """Convert this :class:`.DatetimeExt` to a :class:`.DayFrac`."""
-        days = AD_OFFSET + self._dt.toordinal()
-        day_frac = self.second / CONST.DAYS2SEC
+        days = AD_OFFSET + self.toordinal()
+        day_frac = (self.second + self.sec_frac) / CONST.DAYS2SEC
         day_frac += self.minute / (CONST.DAYS2HOUR * CONST.HOUR2MINUTE)
         day_frac += self.hour / CONST.DAYS2HOUR
-        return DayFrac(days, day_frac)
+        return DayFrac(days + day_frac)
 
 
 class SecDelta:
--- tests/physics/test_stardate_new.py
+++ tests/physics/test_stardate_new.py
@@ -1,21 +1,34 @@
 
+# Standard Library Imports
 from datetime import datetime
 
+# Third Party Imports
+import numpy as np
 import pytest
 
+# RESONAATE Imports
+from resonaate.physics import constants as CONST
 from resonaate.physics.time.stardate_new import DatetimeExt, DayFrac
 
 test_values = [
     (datetime(1776, 7, 4), 2369915.5),
     (datetime(1970, 1, 1, 12), 2440588.0),
-    (datetime(2011, 11, 11, 11, 11), 2455876.965972),
-    (datetime(2024, 12, 31, 23, 59, 59), 2460676.499988),
-    (datetime(2025, 8, 28, 20, 9, 56, 400000), 2460916.340236),
+    (datetime(2011, 11, 11, 11, 11), 2455876.9659722224),
+    (datetime(2024, 12, 31, 23, 59, 59), 2460676.499988426),
+    (datetime(2025, 8, 28, 20, 9, 56, 654321), 2460916.3402390545),
 ]
-"""Retrieved values from <https://aa.usno.navy.mil/data/JulianDate>"""
+"""Computed values using pyerfa@2.0.1.5 :: ``dtf2d()``"""
 
 @pytest.mark.parametrize(("test_dt", "test_jd"), test_values)
 def test_datetimeToDayFrac(test_dt: datetime, test_jd: float):
-    """Make sure :meth:`.DatetimeExt.toDayFrac()` gives same results as :meth:`.datetimeToJulianDate()`."""
-    jd_calc = DatetimeExt(test_dt).toDayFrac()
-    assert abs(jd_calc.days - test_jd) < 1e-6
+    """Verify that :meth:`.DatetimeExt.toDayFrac()` results in the expected value."""
+    jd_calc = DatetimeExt.combine(test_dt.date(), test_dt.time()).toDayFrac()
+    assert abs(jd_calc - test_jd) < np.spacing(jd_calc)
+
+@pytest.mark.parametrize(("test_dt", "test_jd"), test_values)
+def test_dayFracToDatetime(test_dt: datetime, test_jd: float):
+    """Verify that :meth:`.DayFrac.toDatetime()` results in the expected value."""
+    tol_s = np.spacing(test_jd) * CONST.DAYS2SEC  # tolerance in seconds
+    dt_calc = DayFrac(test_jd).toDatetime()
+
+    assert abs((dt_calc - test_dt).total_seconds()) < tol_s
